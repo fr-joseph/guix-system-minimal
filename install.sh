a6287bcf3d0fe8a868765bf5c51da79a2176773f
fj <fj@myr>
2024-11-01 12:20:47 +0100

    add nonguix subs url

--- install.sh
+++ install.sh
@@ -16,14 +16,14 @@ if [ "$1" -eq "1" ]; then
     note "partition the disk"
     gdisk /dev/nvme0n1
     note "now verify the disk is partitioned correctly, and continue with './install.sh 2'"
-elif [ "$1" -eq "2" ]; then    
+elif [ "$1" -eq "2" ]; then
     note "set up luks"
     cryptsetup -v --use-random luksFormat --type luks2 --pbkdf pbkdf2 /dev/nvme0n1p2
     cryptsetup open /dev/nvme0n1p2 main
     note "now verify luks is set up correctly"
     note "then edit 'system.scm' and fill in the UUID from 'cryptsetup luksUUID /dev/nvme0n1p2'"
     note "and then continue with './install.sh 3'"
-elif [ "$1" -eq "3" ]; then    
+elif [ "$1" -eq "3" ]; then
     note "main partition"
     mkfs.ext4 -L main /dev/mapper/main
     mkdir -p /mnt
@@ -40,12 +40,14 @@ elif [ "$1" -eq "3" ]; then
     note "start cow-store"
     herd start cow-store /mnt
 
-    note "now copy the config file to /mnt/etc/system.scm, and then run this command to complete the installation:"
-    note "  guix system init /mnt/etc/system.scm /mnt"
+    note "now copy the config file to /mnt/etc/system.scm, and edit it as needed."
+    note "finish the installation with './install.sh 4'"
     note "and then you can reboot into the new system"
+elif [ "$1" -eq "4" ]; then
+    guix system \
+      --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org' \
+      --cores=0 \
+      init /mnt/etc/system.scm /mnt \
 else
     note "ERROR: Invalid phase number."
 fi
-
-
-
